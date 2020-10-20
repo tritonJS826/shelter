16298758baf876e4a751688470c37c2f446dda89
tritonJS826 <triton.68@mail.ru>
2020-10-20 15:29:35 +0300

    feat: out friends section

---
+++ index.js
@@ -0,0 +1,35 @@
+// // import simpleSlider from "simple-slider";
+//
+// //our-friends__slider
+// const createSliderChild = () => {
+//   const card = document.createElement("div");
+//   card.classList.add("slider-card");
+//
+//   // const cardTitle = document.createElement("h4");
+//   // cardTitle.classList.add("slider-card__name");
+//   //
+//   // const cardImage = document.createElement("img");
+//   // cardImage.classList.add("slider-card__image");
+//   //
+//   // const cardButton = document.createElement("button");
+//   // cardButton.classList.add("button")
+//   // cardButton.classList.add("slider-card__button");
+//   // cardButton.append("Learn more");
+//
+//   return card;
+// }
+//
+// const slider = simpleslider.getSlider({
+//   container: document.getElementById('our-friends__slider'),
+//   children: [createSliderChild(), createSliderChild()],
+//   paused: false,
+//   prop: 'left',
+//   duration: 0.5,
+//   delay: 3,
+//   init: -100,
+//   show: 2,
+//   end: 100,
+//   unit: '%',
+//   onChange: undefined,
+//   onChangeEnd: undefined,
+// });
