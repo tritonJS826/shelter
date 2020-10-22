69dfc9240bc4cf79afd871bd351a5d5116feadbc
tritonJS826 <triton.68@mail.ru>
2020-10-22 19:41:38 +0300

    add: webpack

---
+++ src/pages/ourPets/index2.js
@@ -0,0 +1,16 @@
+// import petsDescrition from "../../pets.js";
+//
+// const PETS_LIST = document.getElementById("pets-list-block__content");
+//
+// petsDescrition.forEach(elem => {
+//   PETS_LIST.insertAdjacentHTML(
+//     "afterbegin",
+//     `
+//    <div class="pet-card">
+//      <div><img src='${elem.img}' alt="pet"  class="pet-card__img"></div>
+//     <p class="pet-card__name">${elem.name}</p>
+//      <button class="button pet-card__button">Learn more</button>
+//    </div>
+//  `
+//   );
+// });
---
+++ webpack.config.js
@@ -0,0 +1,111 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = process.env.NODE_ENV === !isDev;
+// cool theme bundle-analyze-plugin
+
+
+const optimization = () => {
+  const config = {};
+
+  if (isProd) {
+    config.minimizer = [
+      new OptimizeCssAssetsPlugin(),
+      new TerserWebpackPlugin(),
+    ];
+    config.splitChunks = {
+      chunks: 'all',
+    };
+  }
+  return config;
+};
+
+module.exports = {
+  entry: {
+    main: './src/index.js'
+  },
+  output: {
+    filename: '[name].bundle.[hash].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'source-map',
+  optimization: optimization(),
+  module: {
+    rules: [{
+      enforce: 'pre',
+      test: /\.js$/,
+      loader: 'eslint-loader',
+    },
+    {
+      test: /\.js$/,
+      exclude: /(node_modules)/,
+      use: {
+        loader: 'babel-loader',
+      },
+    },
+    {
+      test: /\.css$/,
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: ['css-loader'],
+      }),
+    },
+    {
+      test: /\.scss$/,
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: ['css-loader', 'sass-loader']
+      })
+    },
+    {
+      test: /\.(png|svg|jpg|jpeg|gif)$/,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'img/',
+          },
+        },
+      ],
+    },
+    {
+      test: /\.(ttf|woff|woff2|eot)$/,
+      use: ['file-loader'],
+    },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: true,
+      title: 'movieSearch',
+      favicon: './favicon.ico',
+      minify: {
+        collapseWhitespace: isProd,
+      },
+      myPageHeader: 'movieSearch',
+      template: './src/index.html',
+    }),
+    new ExtractTextPlugin({
+      filename: '[name].style.[hash].css',
+    }),
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin([
+      {
+        from: path.resolve(__dirname, 'favicon.ico'),
+        to: path.resolve(__dirname, 'dist'),
+      },
+    ]),
+  ],
+  devServer: {
+    contentBase: './src',
+    port: 7700,
+    hot: isDev,
+  },
+};
